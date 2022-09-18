868f20001e952cdf55f1649edb63c6bb1211549b
Ilgiz linux <ilgizahiyarov95@gmail.com>
2022-09-18 18:29:52 +0500

    exercise 2 solved

--- main.py
+++ main.py
@@ -19,4 +19,28 @@ def print_recipe(document):
     pprint.pprint(cook_book, width=120, sort_dicts=False)
 
 
-print_recipe("recipes.txt")
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_dict = {}
+    with open('recipes.txt', 'rt', encoding='utf-8') as file:
+        for line in file:
+            if line.strip('\n') in dishes:
+                dish = line
+                count = file.readline()
+                for i in range(int(count)):
+                    row = file.readline()
+                    ing, quantity, measure = row.split(' | ')
+                    quantity = int(quantity) * person_count
+                    if ing in shop_dict.keys():
+                        value = shop_dict[ing]
+                        num = int(value['quantity']) + int(quantity)
+                        
+                        shop_dict[ing] = {'measure': measure.strip('\n'), 'quantity': str(num)}
+                    else:
+                        shop_dict[ing] = {'measure': measure.strip('\n'), 'quantity': str(quantity)}
+    pprint.pprint(shop_dict, sort_dicts=False)
+
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 3)
+#print_recipe("recipes.txt")
+
